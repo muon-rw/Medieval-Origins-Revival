5fe8271662fa6d4ba91e21533215038cd5711c98
muon-rw <pashah012@gmail.com>
2023-11-30 13:58:28 -0600

    Working Icarae using Trinkets functions

--- src/main/java/net/itsparkielad/medievalorigins/MedievalOrigins.java
+++ src/main/java/net/itsparkielad/medievalorigins/MedievalOrigins.java
@@ -15,22 +15,18 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class MedievalOrigins implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LogManager.getLogger("medievalorigins");
 	public static final String MOD_ID = "medievalorigins";
+	/*
 	public static final Predicate<Entity> HAS_WINGS = FabricLoader.getInstance().isModLoaded("origins") ? IcarusOrigins::hasWings : entity -> false;
 	public static final Function<Entity, WingsValues> WINGS = FabricLoader.getInstance().isModLoaded("origins") ? IcarusOrigins.getWingValues() : (entity) -> DefaultWingsValues.INSTANCE;
-
+    */
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 		LOGGER.info("Loading Medieval Origins");
 		ModEnchantments.registerModEnchantments();
-		if(FabricLoader.getInstance().isModLoaded("icarus"))
-			IcarusOrigins.register();
+		// Unused, just using Trinkets equip for now
+		// if(FabricLoader.getInstance().isModLoaded("icarus"))
+		//	IcarusOrigins.register();
 	}
 }
