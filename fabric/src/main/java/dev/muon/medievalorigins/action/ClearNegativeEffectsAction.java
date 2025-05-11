02cf008af0b741dee404613c69badaf149d3d85f
muon-rw <pashah012@gmail.com>
2025-05-11 17:29:42 -0500

    Fix #90 Valkyrie crash (Fabric)

--- fabric/src/main/java/dev/muon/medievalorigins/action/ClearNegativeEffectsAction.java
+++ fabric/src/main/java/dev/muon/medievalorigins/action/ClearNegativeEffectsAction.java
@@ -3,17 +3,22 @@ package dev.muon.medievalorigins.action;
 import dev.muon.medievalorigins.MedievalOrigins;
 import io.github.apace100.apoli.power.factory.action.ActionFactory;
 import io.github.apace100.calio.data.SerializableData;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.effect.MobEffectInstance;
 
+import java.util.List;
+
 public class ClearNegativeEffectsAction {
     public static void action(SerializableData.Instance data, Entity entity) {
         if (entity instanceof LivingEntity livingEntity) {
-            livingEntity.getActiveEffects().stream()
+            List<MobEffect> effectsToRemove = livingEntity.getActiveEffects().stream()
                     .map(MobEffectInstance::getEffect)
                     .filter(effect -> !effect.isBeneficial())
-                    .forEach(livingEntity::removeEffect);
+                    .toList();
+
+            effectsToRemove.forEach(livingEntity::removeEffect);
         }
     }
 
