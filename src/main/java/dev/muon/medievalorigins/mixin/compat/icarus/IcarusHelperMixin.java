c44814d2712a931c0667679b9dcced2f46bc323a
muon-rw <pashah012@gmail.com>
2026-05-22 10:59:42 -0700

    stupid fucking mod lol

--- src/main/java/dev/muon/medievalorigins/mixin/compat/icarus/IcarusHelperMixin.java
+++ src/main/java/dev/muon/medievalorigins/mixin/compat/icarus/IcarusHelperMixin.java
@@ -69,6 +69,21 @@ public abstract class IcarusHelperMixin {
             }
 
             @Override
+            public boolean useStaminaForFlight() {
+                return false;
+            }
+
+            @Override
+            public float staminaAmount() {
+                return 0;
+            }
+
+            @Override
+            public float staminaRegen() {
+                return 0;
+            }
+
+            @Override
             public float requiredFoodAmount() {
                 return 0;
             }
